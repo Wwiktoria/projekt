e66e393355c82caab170d7a7e0ed50e95aef4126
Wwiktoria1 <100830693+Wwiktoria@users.noreply.github.com>
2022-05-26 16:14:30 +0000

    good

--- .ccls-cache/@home@runner@projekt/main.c
+++ .ccls-cache/@home@runner@projekt/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
 
-//ustalam wielkość siatki
-int rzad = 5;
-int kolumna = 4;
+//ustalam wielkosc siatki
+int rzad = 20;
+int kolumna = 20;
 int k=1;
+char s='*';
+char d=' ';
+
 
 //tworzymy granice
 int linia_rzedu(){
 	printf("\n");
-	for(int i=0; i<kolumna; i++){printf(" -----");}
-	printf("\n");
+	//for(int i=0; i<kolumna; i++){printf(" -----");}
+	//printf("\n");*/
 }
 
 //zwracamy ilość żywych sąsiadów
@@ -31,43 +37,47 @@ int linia_rzedu(){
 
 int main(){
 
- do{ 
+
 	  int a[rzad][kolumna], b[rzad][kolumna];
 	  int i,j;
 	  int zywe_komorki_sasiadow;
+	  int c[rzad][kolumna];
 
 	  //tworzę macierz z żywymi i martwymi komórkami (losowa)
 	  for(i=0; i<rzad; i++){
 		  for(j=0;j<kolumna;j++){
 			  a[i][j] = rand() % 2;
+
+
 		  }
 	  }
-	
-	  //wyświetlam macierz
+
+	  /*wyświetlam macierz
 	  printf("Pierwotna generacja:");
-	  linia_rzedu();
+	    linia_rzedu();
 	  for(i=0; i<rzad; i++){
-		  printf(":");
+		  //printf(":");
 		  for(j=0;j<kolumna;j++){
-			  printf(" %d :",a[i][j]);
+			  //printf(" %d :",a[i][j]);
 	  	}
 		  linia_rzedu();
-	  }
-
+	  }*/
+do{
+    a[rzad][kolumna]=c[rzad][kolumna];
 	  //obliczam zależności z żywymi sąsiadami
 	  for(i=0; i<rzad; i++){
 		  for(j=0;j<kolumna;j++){
-			  zywe_komorki_sasiadow =                 licz_ilosc_zywych_sasiadow(a,i,j);
+			  zywe_komorki_sasiadow =licz_ilosc_zywych_sasiadow(a,i,j);
 			  if(a[i][j]==1 && (zywe_komorki_sasiadow==2 ||   zywe_komorki_sasiadow==3)){
-			  	b[i][j]=1;
+			  	b[i][j]=s;
 			  }
 
 			  else if(a[i][j]==0 && zywe_komorki_sasiadow==3){
-				  b[i][j]=1;
+				  b[i][j]=s;
 			  }
 
 			  else{
-				  b[i][j]=0;
+				  b[i][j]=d;
 			  }
 		  }
 	  }
@@ -76,16 +86,19 @@ int main(){
 	  printf("\nNastępna generacja:");
 	  linia_rzedu(rzad);
 	  for(i=0; i<rzad; i++){
-		  printf(":");
+		 // printf(":");
 		  for(j=0;j<kolumna;j++){
-			  printf(" %d :",b[i][j]);
+			  printf(" %c ",b[i][j]);
 		  }
 		  linia_rzedu(rzad);
 	  }
-    
-int c[rzad][kolumna];
+
+
 c[i][j]= b[i][j];
-system("clear");
+Sleep(2000);
+system("cls");
 }while(k>0);
-  
+
 return 0;}
+
+	
